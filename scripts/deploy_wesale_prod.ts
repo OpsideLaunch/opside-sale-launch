7893013f88a4def5520ef2ef62bd0f1490c1edf8
6xiaowu9 <736518585@qq.com>
2023-07-01 15:46:24 +0800

    fix: new contract

--- scripts/deploy_wesale_prod.ts
+++ scripts/deploy_wesale_prod.ts
@@ -49,62 +49,62 @@ async function main() {
   await sleep(1000)
   await wesaleFactory.transferOwnership(ownerAddress)
   console.log('transferOwnership: ', ownerAddress)
-  //   await sleep(5000)
+  await sleep(5000)
 
-  //   const startedAt = Math.floor(new Date().getTime() / 1000)
-  //   const endedAt = startedAt + 3600
-  //   const presalePerPrice = ethers.utils.parseUnits('10', 18)
-  //   const presalePerDexInitPrice = ethers.utils.parseUnits('20', 18)
-  //   const sortCap = ethers.utils.parseUnits('0.5', 18)
-  //   const hardCap = ethers.utils.parseUnits('1', 18)
-  //   const minInvest = ethers.utils.parseUnits('0.1', 18)
-  //   const maxInvest = ethers.utils.parseUnits('1', 18)
-  //   const liquidityRate = ethers.BigNumber.from(700000)
-  //   const firstRelease = ethers.BigNumber.from(300000)
-  //   const cycle = ethers.BigNumber.from(60)
-  //   const cycleRelease = ethers.BigNumber.from(200000)
-  //   const parameters: ParametersStruct = {
-  //     price: presalePerPrice,
-  //     liquidityRate: liquidityRate,
-  //     minInvest: minInvest,
-  //     maxInvest: maxInvest,
-  //     softCap: sortCap,
-  //     hardCap: hardCap,
-  //     router: routerAddress,
-  //     dexInitPrice: presalePerDexInitPrice,
-  //     startedAt: ethers.BigNumber.from(startedAt),
-  //     endedAt: ethers.BigNumber.from(endedAt),
-  //     firstRelease: firstRelease,
-  //     cycle: cycle,
-  //     cycleRelease: cycleRelease,
-  //     investTokenDecimals: ethers.BigNumber.from(0),
-  //   }
-  //   const allowPresaleTokenAmount = presalePerPrice
-  //     .mul(hardCap)
-  //     .div(ethers.utils.parseUnits('1', 18))
-  //     .add(
-  //       hardCap
-  //         .mul(liquidityRate)
-  //         .mul(presalePerDexInitPrice)
-  //         .div(1000000)
-  //         .div(ethers.utils.parseUnits('1', 18))
-  //     )
-  //   await presaleToken
-  //     .connect(founder)
-  //     .approve(wesaleFactory.address, allowPresaleTokenAmount)
+  const startedAt = Math.floor(new Date().getTime() / 1000)
+  const endedAt = startedAt + 3600
+  const presalePerPrice = ethers.utils.parseUnits('10', 18)
+  const presalePerDexInitPrice = ethers.utils.parseUnits('20', 18)
+  const sortCap = ethers.utils.parseUnits('0.5', 18)
+  const hardCap = ethers.utils.parseUnits('1', 18)
+  const minInvest = ethers.utils.parseUnits('0.1', 18)
+  const maxInvest = ethers.utils.parseUnits('1', 18)
+  const liquidityRate = ethers.BigNumber.from(700000)
+  const firstRelease = ethers.BigNumber.from(300000)
+  const cycle = ethers.BigNumber.from(60)
+  const cycleRelease = ethers.BigNumber.from(200000)
+  const parameters: ParametersStruct = {
+    price: presalePerPrice,
+    liquidityRate: liquidityRate,
+    minInvest: minInvest,
+    maxInvest: maxInvest,
+    softCap: sortCap,
+    hardCap: hardCap,
+    router: routerAddress,
+    dexInitPrice: presalePerDexInitPrice,
+    startedAt: ethers.BigNumber.from(startedAt),
+    endedAt: ethers.BigNumber.from(endedAt),
+    firstRelease: firstRelease,
+    cycle: cycle,
+    cycleRelease: cycleRelease,
+    investTokenDecimals: ethers.BigNumber.from(0),
+  }
+  const allowPresaleTokenAmount = presalePerPrice
+    .mul(hardCap)
+    .div(ethers.utils.parseUnits('1', 18))
+    .add(
+      hardCap
+        .mul(liquidityRate)
+        .mul(presalePerDexInitPrice)
+        .div(1000000)
+        .div(ethers.utils.parseUnits('1', 18))
+    )
+  await presaleToken
+    .connect(founder)
+    .approve(wesaleFactory.address, allowPresaleTokenAmount)
 
-  //   //   const tx = await wesaleFactory
-  //   //     .connect(founder)
-  //   //     .createSale(
-  //   //       teamWallet.address,
-  //   //       presaleToken.address,
-  //   //       investTokenA.address,
-  //   //       parameters
-  //   //     )
   //   const tx = await wesaleFactory
   //     .connect(founder)
-  //     .createSale(founder.address, presaleToken.address, investToken, parameters)
-  //   console.log(tx)
+  //     .createSale(
+  //       teamWallet.address,
+  //       presaleToken.address,
+  //       investTokenA.address,
+  //       parameters
+  //     )
+  const tx = await wesaleFactory
+    .connect(founder)
+    .createSale(founder.address, presaleToken.address, investToken, parameters)
+  console.log(tx)
 }
 
 function sleep(ms: number) {
